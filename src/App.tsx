9c04aa00954f0d8578da4e7de4c653aa49c6a649
Primate <bastien.fremondiere@agence-ohana.fr>
2024-07-19 11:05:14 +0200

    Add basic canvas rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { createRef, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const reference = useRef<HTMLCanvasElement>(null);
+
+  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(frameCount % ctx.canvas.width, (frameCount * 10 % (ctx.canvas.height * ctx.canvas.width)) / ctx.canvas.width, 10, 10);
+  }
+
+  useEffect(() => {
+    const canvas = reference.current;
+    const context = canvas?.getContext('2d');
+    let frameCount = 0;
+    let animationFrameId = 0;
+    if (context) {
+      const render = () => {
+        frameCount++;
+        draw(context, frameCount);
+        animationFrameId = window.requestAnimationFrame(render);
+      }
+      render();
+      return () => {
+        window.cancelAnimationFrame(animationFrameId);
+      }
+    }
+  }, [draw])
 
   return (
     <>
@@ -17,6 +42,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <canvas ref={reference} id="canvas" width="150" height="150"></canvas>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
